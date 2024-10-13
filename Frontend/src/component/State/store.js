225346759a6b8ec534665138e8a3f52cd4969aec
Prathamesh <ov01809@gmail.com>
2024-10-13 18:18:47 +0530

    Added orders,restaurantorders,ingredients,cart reducer

--- Frontend/src/component/State/store.js
+++ Frontend/src/component/State/store.js
@@ -4,11 +4,23 @@ import { authReducer } from "./Authentication/Reducer";
 import {thunk} from "redux-thunk"
 import restaurantReducer from "./Restaurant/Reducer";
 import menuItemsReducer from "./Menu/Reducer";
+import cartReducer from "./Cart/Reducer";
+// import orderReducer from "./Order/Reducer";
+import restaurantsOrderReducer from "./Restaurant Order/Reducer";
+import ingredientReducer from "./Ingredients/Reducer";
+import orderReducer from "./order/Reducer";
+// import orderReducer from "./Order/Reducer";
+// import { orderReducer } from "./order/Reducer";
+// import cartReducer from "./order/Reducer";
 const rootReducer=combineReducers(
     {
         auth:authReducer,
         restaurant:restaurantReducer,
-        menu:menuItemsReducer
+        menu:menuItemsReducer,
+        cart:cartReducer,
+        restaurantOrder:restaurantsOrderReducer,
+        ingredients:ingredientReducer,
+        order:orderReducer
 
     }
 )
